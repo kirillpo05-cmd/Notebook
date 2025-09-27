84052690c235a9aa682d3fc5cede2e92ed0d96be
kirillpo05-cmd <kirill.po05@gmail.com>
2025-09-27 14:35:06 +0300

    database

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# подключаем SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# модель пользователя
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+# создаём таблицы (один раз)
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def home():
     return render_template("Window.html")
 
+# обработчик регистрации
+@app.route("/register", methods=["POST"])
+def register():
+    login = request.form["login"]
+    email = request.form["email"]
+    password = request.form["password"]
+
+    # создаём нового пользователя
+    new_user = User(login=login, email=email, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return f"Пользователь {login} успешно зарегистрирован!"
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
